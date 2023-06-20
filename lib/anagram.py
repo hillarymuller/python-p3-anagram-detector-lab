3feefb807e6de88630791fa4e27db5bc0ad52964
Hillary Muller <hillaryaward@gmail.com>
2023-06-20 16:37:26 -0500

    write method to check for anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.letters = sorted(letter for letter in word)
+    
+    def match(self, list):
+        matching = []
+        
+        for word in list:
+            if sorted([letter for letter in word]) == self.letters:
+                matching.append(word)
+        
+        return matching
+            
